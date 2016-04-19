bcd6b6427a2b5bcfb73bd3b907124b6314ab5f28
Bagrat79 <avbago@gmail.com>
2016-04-19 17:39:22 +0400

    Added files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function {
+    var menu=["Pictures","Vodeo","Information"],
+	var i;
+	var text;
+    for(var i = o,i < menu.length, i++ ){
+		document.write("id = demo"  + menu[i] + "<br>");
+	}
+    document.getElementById("demo").innerHTML = text;
+    
+}
